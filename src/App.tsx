23b37a8ee6f5039b822c6a7fc4d6447b8c1e63b8
NguyenRobin <robin.nnguyen@hotmail.com>
2023-10-17 10:42:06 +0200

    s

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,10 @@ function App() {
   return (
     <BrowserRouter basename={import.meta.env.DEV ? '/' : '/interval-app/'}>
       <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<div>sign up</div>} />
+
         <Route element={<AppLayout />}>
-          <Route path="/" element={<Login />} />
-          <Route path="signup" element={<div>sign up</div>} />
           <Route path="stored-passwords" element={<div>password</div>} />
           <Route path="new" element={<div>new credentials</div>} />
           <Route path="edit/:id" element={<div>edit credentials</div>} />
