d425b56e7731459818e30a22de3be052a711dae8
NguyenRobin <robin.nnguyen@hotmail.com>
2023-10-17 10:59:09 +0200

    merged

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/group-exercise-lckd-frontend/'
-})
+  base: '/group-exercise-lckd-frontend/',
+});
