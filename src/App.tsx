7d2a3d9ba6ae2cec233f7b0a1afbc12f3cbf076b
P156HAM <a.hamdi.almasalmeh@gmail.com>
2023-10-18 11:34:55 +0200

    chore: fix inputfield-component, feat: add new-view

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './views/login/Login';
 import AppLayout from './views/appLayout/AppLayout';
+import NewCredential from './views/new/New';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='group-exercise-lckd-frontend'>
       <Routes>
         <Route element={<AppLayout />}>
           <Route path="group-exercise-lckd-frontend/" element={<Login />} />
           <Route path="signup" element={<div>sign up</div>} />
           <Route path="stored-passwords" element={<div>password</div>} />
-          <Route path="new" element={<div>new credentials</div>} />
+          <Route path="new" element={<NewCredential />} />
           <Route path="edit/:id" element={<div>edit credentials</div>} />
         </Route>
       </Routes>
